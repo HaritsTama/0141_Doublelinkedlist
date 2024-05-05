e2dcd2abf3f3310ffcb788558f3521cb5a49aebc
HaritsTama <nayotama1818@gmail.com>
2024-05-05 09:45:05 +0700

    menyelesaikan prosedur retraverse

--- 0141_Doublelinkedlist/0141_Doublelinkedlist.cpp
+++ 0141_Doublelinkedlist/0141_Doublelinkedlist.cpp
@@ -152,5 +152,19 @@ void traverse()
 
 void retraverse()
 {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else
+	{
+		cout << "\nRecord in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
 
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
 }
