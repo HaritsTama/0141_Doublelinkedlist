a723d724f86cc99417a441316bd9243a00cd6621
HaritsTama <nayotama1818@gmail.com>
2024-05-05 09:21:08 +0700

    Membuat code untuk memasukan node baru di awal linked list

--- 0141_Doublelinkedlist/0141_Doublelinkedlist.cpp
+++ 0141_Doublelinkedlist/0141_Doublelinkedlist.cpp
@@ -15,4 +15,27 @@ Node* START = NULL;
 void addNode()
 {
 	Node* newNode = new Node(); //step 1 = create a new node
+	cout << "\nEnter the roll number of the student : ";
+	cin >> newNode->noMhs; //Assign value to the data field of the new node
+	cout << "\nEnter the name of the student : ";
+	cin >> newNode->name; // Assign vaule to the data field of the new node
+
+	// Insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{
+		// step 2 = insert the new node at the beginning
+		if (START != NULL && newNode->noMhs == START->noMhs)
+		{
+			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+			return;
+		}
+		//if the list is empty, make the new node the START
+		newNode->next = START; // step 3 = make the new node point to the first node
+		if (START != NULL)
+		{
+			START->prev = newNode; // step 4 = make the first node poitn to the new node
+		}
+		newNode->prev = NULL; // step 5 = make the new node point to NULL
+		START = newNode; // step 6 = make the new node the first node
+	}
 }
